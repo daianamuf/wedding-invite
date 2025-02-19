fd1cd4c6057795d3bdcfde7d57f639c008808781
Daiana <muflicdaiana@gmail.com.>
2025-02-19 15:27:26 +0200

    add embed map

--- src/App.jsx
+++ src/App.jsx
@@ -270,6 +270,16 @@ function App() {
       </form>
 
       <Countdown />
+
+      <iframe
+        className="map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2738.456426093272!2d23.6715593!3d46.657249199999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47496dd05a55a087%3A0x50bdf632c0197574!2sLight%20House%20by%20SunGarden!5e0!3m2!1sro!2sro!4v1739970710011!5m2!1sro!2sro"
+        width="600"
+        height="400"
+        allowfullscreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </main>
   );
 }
